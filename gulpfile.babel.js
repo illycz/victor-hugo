f193f36546ed7a574cbe2eaeb6e142f59460f81d
Patrik Illy <patrik.illy@gmail.com>
2017-11-30 14:16:48 +0100

    Sass support

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,14 @@
 import gulp from "gulp";
+import gulpLoadPlugins from "gulp-load-plugins";
 import {spawn} from "child_process";
 import hugoBin from "hugo-bin";
 import gutil from "gulp-util";
-import postcss from "gulp-postcss";
-import cssImport from "postcss-import";
-import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
 import watch from "gulp-watch";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
 
+const $ = gulpLoadPlugins();
 const browserSync = BrowserSync.create();
 
 // Hugo arguments
@@ -21,13 +20,35 @@ gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, hugoArgsPreview));
 
 // Build/production tasks
-gulp.task("build", ["css", "js"], (cb) => buildSite(cb, [], "production"));
-gulp.task("build-preview", ["css", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
+gulp.task("build", ["css:production", "js"], (cb) => buildSite(cb, [], "production"));
+gulp.task("build-preview", ["css:production", "js"], (cb) => buildSite(cb, hugoArgsPreview, "production"));
 
-// Compile CSS with PostCSS
-gulp.task("css", () => (
-  gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
+// Compile CSS with Sass
+gulp.task("css:development", () => (
+  gulp.src("./src/css/*.scss")
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.sass.sync({
+      outputStyle: "expanded",
+      precision: 10,
+      includePaths: ["."]
+    }).on("error", $.sass.logError))
+    .pipe($.autoprefixer({browsers: ["> 1%", "last 2 versions", "Firefox ESR"]}))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest("./dist/css"))
+    .pipe(browserSync.stream())
+));
+
+gulp.task("css:production", () => (
+  gulp.src("./src/css/*.scss")
+    .pipe($.plumber())
+    .pipe($.sass.sync({
+      outputStyle: "expanded",
+      precision: 10,
+      includePaths: ["."]
+    }).on("error", $.sass.logError))
+    .pipe($.autoprefixer({browsers: ["> 1%", "last 2 versions", "Firefox ESR"]}))
+    .pipe($.cssnano({safe: true, autoprefixer: false}))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
 ));
@@ -48,15 +69,15 @@ gulp.task("js", (cb) => {
 });
 
 // Development server with browsersync
-gulp.task("server", ["hugo", "css", "js"], () => {
+gulp.task("server", ["hugo", "css:development", "js"], () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
     }
   });
-  watch("./src/js/**/*.js", () => { gulp.start(["js"]) });
-  watch("./src/css/**/*.css", () => { gulp.start(["css"]) });
-  watch("./site/**/*", () => { gulp.start(["hugo"]) });
+  watch("./src/js/**/*.js", () => { gulp.start(["js"]); });
+  watch("./src/css/**/*.scss", () => { gulp.start(["css:development"]); });
+  watch("./site/**/*", () => { gulp.start(["hugo"]); });
 });
 
 /**
